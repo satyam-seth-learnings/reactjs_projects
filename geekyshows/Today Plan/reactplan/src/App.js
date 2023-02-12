148e6be7ebf1582c971d682152a80974094859c0
satyam-seth <satyam1998.1998@gmail.com>
2023-02-12 12:14:19 +0530

    format today plan project

--- geekyshows/Today Plan/reactplan/src/App.js
+++ geekyshows/Today Plan/reactplan/src/App.js
@@ -22,47 +22,47 @@ class App extends Component {
 
   handleDelete = id => {
     console.log("Deleted", id);
-    const Olditems = [...this.state.items]
-    console.log("Olditems", Olditems);
-    const items = Olditems.filter((element, i) => {
+    const OldItems = [...this.state.items]
+    console.log("OldItems", OldItems);
+    const items = OldItems.filter((element, i) => {
       return i !== id
     })
-    console.log("Newitems", items);
+    console.log("NewItems", items);
     this.setState({ items: items });
   }
 
   render() {
     return (
       <div className="container-fluid my-5">
-      <div className="row">
-        <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
-          <h2 className="text-center"> Today's Plan </h2>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-9">
-                <input type="text" className="form-control" placeholder="Write Plan Here" value={this.state.text} onChange={this.handleChange} />
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+            <h2 className="text-center"> Today's Plan </h2>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-9">
+                  <input type="text" className="form-control" placeholder="Write Plan Here" value={this.state.text} onChange={this.handleChange} />
+                </div>
+                <div className="col-2">
+                  <button className="btn btn-warning px-5 font-weight-bold" onClick={this.handleAdd}>Add</button>
+                </div>
               </div>
-              <div className="col-2">
-                <button className="btn btn-warning px-5 font-weight-bold" onClick={this.handleAdd}>Add</button>
-              </div>
-            </div>
-            <div className="conatiner">
-              <ul className="list-unstyled row m-5">
-                {
-                  this.state.items.map((value, i) => {
-                    return <Plan key={i} id={i} value={value} sendData={this.handleDelete} />
-                  })
-                }
-              </ul>
-              {/* <ul className="list-unstyled row m-5">
+              <div className="container">
+                <ul className="list-unstyled row m-5">
+                  {
+                    this.state.items.map((value, i) => {
+                      return <Plan key={i} id={i} value={value} sendData={this.handleDelete} />
+                    })
+                  }
+                </ul>
+                {/* <ul className="list-unstyled row m-5">
                 <Plan p={this.state.items} sendData={this.handleDelete} />
               </ul> */}
+              </div>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
-    </div>
     )
   }
 }
