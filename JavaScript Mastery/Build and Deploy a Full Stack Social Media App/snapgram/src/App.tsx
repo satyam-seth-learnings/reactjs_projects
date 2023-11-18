5e87512afcb556f97ced59482e304c5877f5574e
satyam-seth <satyam1998.1998@gmail.com>
2023-11-19 01:44:49 +0530

    setup tailwind

--- JavaScript Mastery/Build and Deploy a Full Stack Social Media App/snapgram/src/App.tsx
+++ JavaScript Mastery/Build and Deploy a Full Stack Social Media App/snapgram/src/App.tsx
@@ -1,6 +1,12 @@
+import './global.css'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+        Hello Sanpgram!
+        </h1>
+    </div>
   )
 }
 
