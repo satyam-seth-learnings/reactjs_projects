80d6555f177dd96ac96ca9c203f3bfeea88f5d2f
satyam-seth <satyam1998.1998@gmail.com>
2023-12-03 10:47:44 +0530

    add useSignInAccount mutation

--- JavaScript Mastery/Build and Deploy a Full Stack Social Media App/snapgram/src/lib/react-query/queriesAndMutations.ts
+++ JavaScript Mastery/Build and Deploy a Full Stack Social Media App/snapgram/src/lib/react-query/queriesAndMutations.ts
@@ -1,9 +1,16 @@
 import { INewUser } from "@/types";
 import { useMutation } from "@tanstack/react-query";
-import { createUserAccount } from "../appwright/api";
+import { createUserAccount, signInAccount } from "../appwright/api";
 
 export const useCreateUserAccountMutation = () => {
   return useMutation({
     mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSingInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
   });
 };
